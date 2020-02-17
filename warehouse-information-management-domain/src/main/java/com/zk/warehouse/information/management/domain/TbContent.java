2d20aad87b6a5a9a6b77a139506e988d995a4209
cauliflower <1214159039@qq.com>
2020-02-17 12:45:27 +0800

    内容

---
+++ warehouse-information-management-domain/src/main/java/com/zk/warehouse/information/management/domain/TbContent.java
@@ -0,0 +1,11 @@
+package com.zk.warehouse.information.management.domain;
+
+import com.zk.warehouse.information.management.commons.persistence.BaseEntity;
+
+/**
+ * @author zk
+ * @date 2020/2/17-12:43
+ */
+public class TbContent extends BaseEntity {
+    
+}
